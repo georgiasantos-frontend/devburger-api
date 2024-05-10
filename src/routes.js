5464e2dbc20b3a5d74edd779b3bfe42716f21736
georgiasantos-frontend <georgiasantos.frontend@gmail.com>
2024-05-10 19:44:41 +0100

    first commit

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const routes = new Router();
+
+routes.get("/", (request, response) => {
+  return response.status(200).json({ message: "Hello world" });
+});
+export default routes;
